84a3a24f83f49bf536fd9ba3f3a90031ff1b97e7
dimuthu <dimuthu.jayamanna@gmail.com>
2013-08-11 23:17:30 +0530

    Refactoring

--- HelloOpenCV/src/com/hello/DetectSquares.java
+++ HelloOpenCV/src/com/hello/DetectSquares.java
@@ -7,29 +7,85 @@ import org.opencv.core.MatOfInt;
 import org.opencv.core.MatOfPoint;
 import org.opencv.core.MatOfPoint2f;
 import org.opencv.core.Point;
-import org.opencv.core.Scalar;
-import org.opencv.highgui.Highgui;
 import org.opencv.imgproc.Imgproc;
 
-import android.util.Log;
-
-import java.io.File;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class DetectSquares {
+	public static int contourIdx = -1;
+	
+	/**
+	 * http://stackoverflow.com/questions/18020455/java-opencv-tesseract-ocr-code-regocnition/18042054#18042054
+	 * @param src
+	 * @return
+	 * @throws Exception
+	 */
+	public static MatOfPoint find(Mat src) {
+        Mat blurred = src.clone();
+        Imgproc.medianBlur(src, blurred, 9);
 
-	int thresh = 50, N = 11;
-	static Mat blurred;
-	static Mat gray;
-	static Mat gray0;
+        Mat gray0 = new Mat(blurred.size(), CvType.CV_8U), gray = new Mat();
 
-	static MatOfInt fromTo;
-	static Point point;
-	static List<MatOfPoint> contours;
-	static MatOfPoint2f approx;
-	static int threshold_level = 2;
+        List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
+
+        List<Mat> blurredChannel = new ArrayList<Mat>();
+        blurredChannel.add(blurred);
+        List<Mat> gray0Channel = new ArrayList<Mat>();
+        gray0Channel.add(gray0);
+
+        MatOfPoint2f approxCurve;
+
+        double maxArea = 0;
+
+        for (int c = 0; c < 3; c++) {
+            int ch[] = {c, 0};
+            Core.mixChannels(blurredChannel, gray0Channel, new MatOfInt(ch));
+
+            int thresholdLevel = 1;
+            for (int t = 0; t < thresholdLevel; t++) {
+                if (t == 0) {
+                    Imgproc.Canny(gray0, gray, 10, 20, 3, true); // true ?
+                    Imgproc.dilate(gray, gray, new Mat(), new Point(-1, -1), 1); // 1 ?
+                } else {
+                    Imgproc.adaptiveThreshold(gray0, gray, thresholdLevel, Imgproc.ADAPTIVE_THRESH_GAUSSIAN_C, Imgproc.THRESH_BINARY, (src.width() + src.height()) / 200, t);
+                }
+
+                Imgproc.findContours(gray, contours, new Mat(), Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE);
+
+                for (MatOfPoint contour : contours) {
+                	//Log.i("DIMUTHU::", "Contour size is : " + contours.size());
+                    MatOfPoint2f temp = new MatOfPoint2f(contour.toArray());
+
+                    double area = Imgproc.contourArea(contour);
+                    approxCurve = new MatOfPoint2f();
+                    Imgproc.approxPolyDP(temp, approxCurve, Imgproc.arcLength(temp, true) * 0.02, true);
+
+                    if (approxCurve.total() == 4 && area >= maxArea) {
+                        double maxCosine = 0;
+
+                        List<Point> curves = approxCurve.toList();
+                        for (int j = 2; j < 5; j++)
+                        {
+                            double cosine = Math.abs(angle(curves.get(j % 4), curves.get(j - 2), curves.get(j - 1)));
+                            maxCosine = Math.max(maxCosine, cosine);
+                        }
+
+                        if (maxCosine < 0.3) {
+                            maxArea = area;
+                            contourIdx = contours.indexOf(contour);
+                            //contours.set(maxId, getHull(contour));
+                        }
+                    }
+                }
+            }
+        }
+
+        if (contourIdx >= 0) {
+        	return contours.get(contourIdx);
+        }
+        return null;
+    }
 
 	// helper function:
 	// finds a cosine of angle between vectors
@@ -43,179 +99,5 @@ public class DetectSquares {
 		return (dx1 * dx2 + dy1 * dy2)
 				/ Math.sqrt((dx1 * dx1 + dy1 * dy1) * (dx2 * dx2 + dy2 * dy2)
 						+ 1e-10);
-	}
-
-	public static void prepare() {
-		blurred = new Mat();
-		gray = new Mat();
-		contours = new ArrayList<MatOfPoint>();
-		point = new Point(-1, -1);
-		approx = new MatOfPoint2f();
-
-	}
-
-	/**
-	 * http://stackoverflow.com/questions/8667818/opencv-c-obj-c-detecting-a-sheet-of-paper-square-detection/8863060#8863060%29Regarding
-	 * @param image
-	 * @param squares
-	 */
-	public static void findSquares(Mat image, ArrayList<List<Point>> squares) {
-		// blur will enhance edge detection
-		Imgproc.medianBlur(image, blurred, 9);
-
-		gray0 = new Mat(blurred.size(), CvType.CV_8U);
-
-		// find squares in every color plane of the image
-		for (int c = 0; c < 3; c++) {
-			int ch[] = { c, 0 };
-
-			List<Mat> src = new ArrayList<Mat>();
-			src.add(blurred);
-			List<Mat> dest = new ArrayList<Mat>();
-			dest.add(gray0);
-
-			fromTo = new MatOfInt(ch);
-			Core.mixChannels(src, dest, fromTo);
-
-			for (int l = 0; l < threshold_level; l++) {
-				// Use Canny instead of zero threshold level! Canny helps to
-				// catch squares with gradient shading
-				if (l == 0) {
-					Imgproc.Canny(gray0, gray, 10, 20, 3, false);
-
-					// Dilate helps to remove potential holes between edge segments
-					Imgproc.dilate(gray, gray, new Mat(), point, 1);
-				} else {
-					int i = (l + 1) * 255 / threshold_level;
-					gray = gray0.rows() >= i ? gray0 : gray; // TODO
-				}
-
-				// Find contours and store them in a list.
-				Imgproc.findContours(gray, contours, new Mat(),
-						Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE);
-
-				// Test contours
-				findAndPopulateSquares(squares, contours);
-
-				// drawContours(image, contours);
-			}
-		}
-	}
-	
-	/**
-	 * http://stackoverflow.com/questions/18020455/java-opencv-tesseract-ocr-code-regocnition/18042054#18042054
-	 * @param src
-	 * @return
-	 * @throws Exception
-	 */
-	public static MatOfPoint find(Mat src) throws Exception {
-	        Mat blurred = src.clone();
-	        Imgproc.medianBlur(src, blurred, 9);
-
-	        Mat gray0 = new Mat(blurred.size(), CvType.CV_8U), gray = new Mat();
-
-	        List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
-
-	        List<Mat> blurredChannel = new ArrayList<Mat>();
-	        blurredChannel.add(blurred);
-	        List<Mat> gray0Channel = new ArrayList<Mat>();
-	        gray0Channel.add(gray0);
-
-	        MatOfPoint2f approxCurve;
-
-	        double maxArea = 0;
-	        int maxId = -1;
-
-	        for (int c = 0; c < 3; c++) {
-	            int ch[] = {c, 0};
-	            Core.mixChannels(blurredChannel, gray0Channel, new MatOfInt(ch));
-
-	            int thresholdLevel = 1;
-	            for (int t = 0; t < thresholdLevel; t++) {
-	                if (t == 0) {
-	                    Imgproc.Canny(gray0, gray, 10, 20, 3, true); // true ?
-	                    Imgproc.dilate(gray, gray, new Mat(), new Point(-1, -1), 1); // 1 ?
-	                } else {
-	                    Imgproc.adaptiveThreshold(gray0, gray, thresholdLevel, Imgproc.ADAPTIVE_THRESH_GAUSSIAN_C, Imgproc.THRESH_BINARY, (src.width() + src.height()) / 200, t);
-	                }
-
-	                Imgproc.findContours(gray, contours, new Mat(), Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE);
-
-	                for (MatOfPoint contour : contours) {
-	                	//Log.i("DIMUTHU::", "Contour size is : " + contours.size());
-	                    MatOfPoint2f temp = new MatOfPoint2f(contour.toArray());
-
-	                    double area = Imgproc.contourArea(contour);
-	                    approxCurve = new MatOfPoint2f();
-	                    Imgproc.approxPolyDP(temp, approxCurve, Imgproc.arcLength(temp, true) * 0.02, true);
-
-	                    if (approxCurve.total() == 4 && area >= maxArea) {
-	                        double maxCosine = 0;
-
-	                        List<Point> curves = approxCurve.toList();
-	                        for (int j = 2; j < 5; j++)
-	                        {
-	                            double cosine = Math.abs(angle(curves.get(j % 4), curves.get(j - 2), curves.get(j - 1)));
-	                            maxCosine = Math.max(maxCosine, cosine);
-	                        }
-
-	                        if (maxCosine < 0.3) {
-	                            maxArea = area;
-	                            maxId = contours.indexOf(contour);
-	                            //contours.set(maxId, getHull(contour));
-	                        }
-	                    }
-	                }
-	            }
-	        }
-
-	        if (maxId >= 0) {
-	        	MatOfPoint mmm = contours.get(maxId);
-	            //Imgproc.drawContours(src, mmm, maxId, new Scalar(255, 0, 0, .8), 8);
-	            
-	            return mmm;
-	        }
-	        return null;
-	    }
-
-	private static void findAndPopulateSquares(ArrayList<List<Point>> squares, List<MatOfPoint> contours) {
-		
-		for (int i = 0; i < contours.size(); i++) {
-			// approximate contour with accuracy proportional to the contour perimeter.
-			MatOfPoint mapOfPoint = contours.get(i);
-			MatOfPoint2f mapOfPoint2f = new MatOfPoint2f();
-
-			mapOfPoint2f.fromArray(mapOfPoint.toArray());
-
-			double epilson = Imgproc.arcLength(mapOfPoint2f, true);
-			epilson *= 0.02;
-			Imgproc.approxPolyDP(mapOfPoint2f, approx, epilson, true);
-
-			// Note: absolute value of an area is used because
-			// area may be positive or negative - in accordance with the
-			// contour orientation
-
-			MatOfPoint approx_mapofpoint = new MatOfPoint(approx.toArray());
-
-			if (approx.size().area() == 4
-					&& Math.abs(Imgproc.contourArea(approx)) > 1000
-					&& Imgproc.isContourConvex(approx_mapofpoint)) {
-				double maxCosine = 0;
-
-				for (int j = 2; j < 5; j++) {
-					double cosine = Math.abs(angle(approx.toArray()[j % 4],
-							approx.toArray()[j - 2], approx.toArray()[j - 1]));
-					maxCosine = Math.max(maxCosine, cosine);
-				}
-
-				if (maxCosine < 0.3) {
-					squares.add(approx.toList());
-				}
-			}
-		}
-	}
-
-	private void drawContours(Mat image, List<MatOfPoint> contours) {
-		Imgproc.drawContours(image, contours, -1, new Scalar(0, 255, 0));
 	}
 }
--- HelloOpenCV/src/com/hello/HelloOpenCvActivity.java
+++ HelloOpenCV/src/com/hello/HelloOpenCvActivity.java
@@ -1,6 +1,5 @@
 package com.hello;
 
-import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,21 +9,14 @@ import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
 import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
-import org.opencv.android.Utils;
-import org.opencv.core.Core;
-import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
-import org.opencv.core.Point;
-import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.highgui.Highgui;
 import org.opencv.imgproc.Imgproc;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.util.Log;
 import android.view.Menu;
 import android.view.SurfaceView;
@@ -32,13 +24,13 @@ import android.view.WindowManager;
 
 public class HelloOpenCvActivity extends Activity implements CvCameraViewListener2 {
 
-    protected static final String TAG = "DIMUTHU::";
-    CameraBridgeViewBase mOpenCvCameraView;
-    Mat square2;
-    int counter = 0;
+	protected static final String TAG = "DIMUTHU::";
+	CameraBridgeViewBase mOpenCvCameraView;
+	Mat square2;
+	int counter = 0;
 
 	@Override
-    protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState) {
 		Log.i(TAG, "called onCreate");
 		super.onCreate(savedInstanceState);
 		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
@@ -46,123 +38,69 @@ public class HelloOpenCvActivity extends Activity implements CvCameraViewListene
 		mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.HelloOpenCvView);
 		mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
 		mOpenCvCameraView.setCvCameraViewListener(this);
-    }
-	
+	}
+
 	@Override
-	public void onPause()
-	{
+	public void onPause() {
 		super.onPause();
 		if (mOpenCvCameraView != null)
 			mOpenCvCameraView.disableView();
 	}
-	
+
 	public void onDestroy() {
 		super.onDestroy();
 		if (mOpenCvCameraView != null)
 			mOpenCvCameraView.disableView();
 	}
-	
+
 	public void onCameraViewStarted(int width, int height) {
-		DetectSquares.prepare();
 	}
-	
+
 	public void onCameraViewStopped() {
 	}
-	
+
 	public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
-		//if (counter++ % 10 == 0) {
-			
-			Mat mRgba = inputFrame.rgba();
-			
-//			ArrayList<List<Point>> squares = new ArrayList<List<Point>>();
-//			DetectSquares.findSquares(mRgba, squares);
-//			if (squares.size() > 0) {
-//				Log.i(TAG, "Square count is : " + squares.size());
-//				drawSquares(mRgba, squares);
-//			}
-			try {
-				List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
-				MatOfPoint contour = DetectSquares.find(mRgba);
-				contours.add(contour);
-				
-				Imgproc.drawContours(mRgba, contours, -1, new Scalar(0,255,0), Core.FILLED);
-//				 Imgproc.drawContours(src, contours, maxId, new Scalar(255, 0, 0, .8), 8);
-				Log.i(TAG, "Point area : " + contour.size().area());
-			} catch (Exception e) {
-//				Log.e(TAG, e.getMessage());
-			}
-			
-		//}
-		//else {
-		//	return inputFrame.rgba();
-		//}
-	        
-			//test 1
-//		Mat mRgba = inputFrame.gray();
-//		Core.rectangle(mRgba, new Point(3, 4), new Point(7, 8), new Scalar(0,255,0));
-//        List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
-//        Imgproc.findContours(mRgba, contours, new Mat(), Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
-//
-//        Imgproc.drawContours(mRgba, contours, -1, new Scalar(255,0,0));
-//        Log.i(TAG, "contour count " + contours.size());
-			
-			// test 2
-//			Mat blurred = new Mat(mRgba.size(), mRgba.type());
-//			Imgproc.medianBlur(mRgba, blurred, 9);
-//			return blurred;
-        return mRgba;
-		
+		Mat mRgba = inputFrame.rgba();
+
+		List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
+		contours.add(DetectSquares.find(mRgba));
+		if (contours.get(0) != null) {
+			Imgproc.drawContours(mRgba, contours, -1/*TODO*/, new Scalar(0, 255, 0), 4);
+		}
+
+		return mRgba;
+
 	}
-	
-	// the function draws all the squares in the image
-	private void drawSquares(Mat image, ArrayList<List<Point>> squares)
-	{
-		List<MatOfPoint> polyline = null;
-	    for(int i = 0; i < squares.size(); i++ )
-	    {
-	        Point point = squares.get(i).get(0);
-	                
-	        Log.i(TAG, "Points x : " + point.x + " y: " + point.y);
-	        
-	        polyline = new ArrayList<MatOfPoint>();
-	        polyline.add(new MatOfPoint(point)); //TODO
-	    }
-	    
-	    if (polyline != null) {
-	    	Core.polylines(image, polyline, true, new Scalar(255, 0, 0));
-	    	Imgproc.drawContours(image, polyline, -1, new Scalar(0,255,0), Core.FILLED);
-	    }
-	}
-	
+
 	@Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.hello_open_cv, menu);
-        return true;
-    }
-    
-    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
-    	@Override
-    	public void onManagerConnected(int status) {
-    		switch (status) {
-    			case LoaderCallbackInterface.SUCCESS:
-    			{
-    				Log.i(TAG, "OpenCV loaded successfully");
-    				mOpenCvCameraView.enableView();
-    				square2 = Highgui.imread("/mnt/sdcard/square2.jpg");
-    			} break;
-    			default:
-    			{
-    				super.onManagerConnected(status);
-    			} break;
-    		}
-    	}
-    };
-    	
-	 @Override
-	 public void onResume()
-	 {
-		 super.onResume();
-		 OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_6, this, mLoaderCallback);
-	 }
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.hello_open_cv, menu);
+		return true;
+	}
+
+	private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+		@Override
+		public void onManagerConnected(int status) {
+			switch (status) {
+			case LoaderCallbackInterface.SUCCESS: {
+				Log.i(TAG, "OpenCV loaded successfully");
+				mOpenCvCameraView.enableView();
+				square2 = Highgui.imread("/mnt/sdcard/square2.jpg");
+			}
+				break;
+			default: {
+				super.onManagerConnected(status);
+			}
+				break;
+			}
+		}
+	};
+
+	@Override
+	public void onResume() {
+		super.onResume();
+		OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_6, this,
+				mLoaderCallback);
+	}
 }
