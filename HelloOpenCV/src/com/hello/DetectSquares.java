d94ccbd7d41c9a9a531f6dfed2ada3b58bc2ff2f
dimuthu <dimuthu.jayamanna@gmail.com>
2013-08-11 13:11:27 +0530

    Square counting started

---
+++ HelloOpenCV/src/com/hello/DetectSquares.java
@@ -0,0 +1,123 @@
+package com.hello;
+
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfInt;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.MatOfPoint2f;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.highgui.Highgui;
+import org.opencv.imgproc.Imgproc;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class DetectSquares {
+    
+    int thresh = 50, N = 11;
+
+    // helper function:
+// finds a cosine of angle between vectors
+// from pt0->pt1 and from pt0->pt2
+    static double angle(Point pt1, Point pt2, Point pt0) {
+        double dx1 = pt1.x - pt0.x;
+        double dy1 = pt1.y - pt0.y;
+        double dx2 = pt2.x - pt0.x;
+        double dy2 = pt2.y - pt0.y;
+
+        return (dx1 * dx2 + dy1 * dy2) / Math.sqrt((dx1 * dx1 + dy1 * dy1) * (dx2 * dx2 + dy2 * dy2) + 1e-10);
+    }
+
+    public void find_squares(Mat image, ArrayList<List<Point>> squares) {
+    	 Imgproc img = new Imgproc();
+
+         // blur will enhance edge detection
+         org.opencv.core.Mat blurred = new org.opencv.core.Mat();
+         Imgproc.medianBlur(image, blurred, 9);
+
+         Mat gray0 = new Mat(blurred.size(), CvType.CV_8U);
+         Mat gray = new Mat();
+
+         List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
+
+         // find squares in every color plane of the image
+         for (int c = 0; c < 3; c++) {
+             int ch[] = {c, 0};
+
+             List<Mat> src = new ArrayList<Mat>();
+             src.add(blurred);
+             List<Mat> dest = new ArrayList<Mat>();
+             dest.add(gray0);
+
+             MatOfInt a = new MatOfInt(ch);
+             Core.mixChannels(src, dest, a);
+
+             // try several threshold levels
+             final int threshold_level = 2;
+             for (int l = 0; l < threshold_level; l++) {
+                 // Use Canny instead of zero threshold level!
+                 // Canny helps to catch squares with gradient shading
+                 if (l == 0) {
+                     Imgproc.Canny(gray0, gray, 10, 20, 3, false);
+
+                     // Dilate helps to remove potential holes between edge segments
+                     Point point = new Point(-1, -1);
+                     Imgproc.dilate(gray, gray, new Mat(), point, 1);
+                 } else {
+                     int i = (l + 1) * 255 / threshold_level;
+                     gray = gray0.cols() >= i ? gray0 : gray; //TODO
+
+                 }
+
+                 // Find contours and store them in a list. 
+                 Imgproc.findContours(gray, contours, new Mat(), Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE);
+                 //drawContours(image, contours);
+
+                 // Test contours
+                 MatOfPoint2f approx = new MatOfPoint2f();
+                 for (int i = 0; i < contours.size(); i++) {
+                     // approximate contour with accuracy proportional
+                     // to the contour perimeter
+                	 MatOfPoint mapOfPoint = contours.get(i);
+                	 MatOfPoint2f mapOfPoint2f = new MatOfPoint2f();
+                	              	 
+                	 mapOfPoint2f.fromArray(mapOfPoint.toArray());
+                	 
+                     double epilson = Imgproc.arcLength(mapOfPoint2f, true);
+                     epilson *= 0.02;
+                     Imgproc.approxPolyDP(mapOfPoint2f, approx, epilson, true);
+
+                     // Note: absolute value of an area is used because
+                     // area may be positive or negative - in accordance with the
+                     // contour orientation
+
+                     MatOfPoint approx_mapofpoint = new MatOfPoint(approx.toArray());
+
+                     if (approx.size().area() == 4 &&
+                             Math.abs(Imgproc.contourArea(approx)) > 1000 &&
+                             Imgproc.isContourConvex(approx_mapofpoint)) {
+                         double maxCosine = 0;
+
+                         for (int j = 2; j < 5; j++) {
+                             double cosine = Math.abs(angle(approx.toArray()[j % 4], approx.toArray()[j - 2], approx.toArray()[j - 1]));
+                             maxCosine = Math.max(maxCosine, cosine);
+                         }
+
+                         if (maxCosine < 0.3) {
+                             squares.add(approx.toList());
+                         }
+                     }
+                 }
+             }
+         }
+
+     }
+    
+    private void drawContours(Mat image, List<MatOfPoint> contours) {
+    	 Imgproc.drawContours(image, contours, -1, new Scalar(255,0,0,255));
+    }
+}
--- HelloOpenCV/src/com/hello/HelloOpenCvActivity.java
+++ HelloOpenCV/src/com/hello/HelloOpenCvActivity.java
@@ -1,15 +1,30 @@
 package com.hello;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
 import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.highgui.Highgui;
+import org.opencv.imgproc.Imgproc;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.util.Log;
 import android.view.Menu;
 import android.view.SurfaceView;
@@ -19,6 +34,9 @@ public class HelloOpenCvActivity extends Activity implements CvCameraViewListene
 
     protected static final String TAG = "DIMUTHU::";
     CameraBridgeViewBase mOpenCvCameraView;
+    Mat image;
+    private Mat mRgba15;
+    DetectSquares detectSquares = new DetectSquares();
 
 	@Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,14 +64,74 @@ public class HelloOpenCvActivity extends Activity implements CvCameraViewListene
 	}
 	
 	public void onCameraViewStarted(int width, int height) {
+		this.prepare(width, height);
 	}
 	
 	public void onCameraViewStopped() {
 	}
 	
 	public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
+		//mOpenCvCameraView.disableView();
+//		Log.i(TAG, "Re-drawing image");
+//		Mat mRgba = inputFrame.rgba();
+//		Mat mGray = new Mat();
+//        Imgproc.cvtColor(mRgba, mGray, Imgproc.COLOR_BGRA2GRAY);
+//        Mat xyz = image.clone();
+		//Utils.
+		ArrayList<List<Point>> squares = new ArrayList<List<Point>>();
+		detectSquares.find_squares(inputFrame.rgba(), squares);
+		if (squares.size() > 0) {
+			Log.i(TAG, "Square count is : " + squares.size());
+		}
+		
+//		Mat blurred = new Mat(image.size(), CvType.CV_64FC4);
+//		Imgproc.medianBlur(image, blurred, 9);
+	        
 		return inputFrame.rgba();
 	}
+	
+	// the function draws all the squares in the image
+	private void drawSquares(Mat image, ArrayList<List<Point>> squares)
+	{
+	    for(int i = 0; i < squares.size(); i++ )
+	    {
+	        Point point = squares.get(i).get(0);
+	        int n = (int)squares.get(i).size();
+	        
+	        List<MatOfPoint> polyline = new ArrayList<MatOfPoint>();
+	        polyline.add(new MatOfPoint(point)); //TODO
+	        
+	        Core.polylines(image, polyline, true, new Scalar(100));
+	        //polylines(image, &p, &n, 1, true, Scalar(0,255,0), 3, CV_AA);
+	    }
+
+	   
+	    
+//	    Highgui.im
+//	    imshow(wndname, image);
+	}
+	
+	private Bitmap ReadImage1(String fBitmap) {
+//	    String root = Environment.getExternalStorageDirectory().toString();
+//	    File myDir = new File(root + "/preprocessed");
+	    File file = new File(fBitmap); //or any other format supported
+	   // UIHelper.displayText(this, R.id.textView1, file.toString());
+	    Bitmap bitmap = null; 
+
+	    try {
+	        BitmapFactory.Options options = new BitmapFactory.Options();
+	        options.inPreferredConfig = Bitmap.Config.ARGB_8888;        
+	        bitmap = BitmapFactory.decodeFile(file.getAbsolutePath(),options); //This gets the image `       
+	        return bitmap;
+
+	        } catch (Exception e) {
+	               e.printStackTrace();
+	               //UIHelper.displayText(this, R.id.textView1, "Doesn't exist");
+	        }
+
+	        return bitmap;
+	 }
+	
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -70,6 +148,7 @@ public class HelloOpenCvActivity extends Activity implements CvCameraViewListene
     			{
     				Log.i(TAG, "OpenCV loaded successfully");
     				mOpenCvCameraView.enableView();
+    				image = Highgui.imread("/mnt/sdcard/square2.jpg");
     			} break;
     			default:
     			{
@@ -84,5 +163,13 @@ public class HelloOpenCvActivity extends Activity implements CvCameraViewListene
 	 {
 		 super.onResume();
 		 OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_6, this, mLoaderCallback);
+	 }
+	 
+	 /* This method is to make the processor know the size of the frames that
+	     * will be delivered via puzzleFrame.
+	     * If the frames will be different size - then the result is unpredictable
+	     */
+	 public synchronized void prepare(int width, int height) {
+		 mRgba15 = new Mat(height, width, CvType.CV_8UC4);
 	 }
 }
